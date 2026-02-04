d1c5f0088605a8db2e17d88bec23218f77ed7400
ibraheem-111 <ibraheemsal@outlook.com>
2026-02-03 21:48:29 -0700

    to get yaml to work

--- assignments/first_order_boustrophedon_navigator/launch/boustrophedon.launch.py
+++ assignments/first_order_boustrophedon_navigator/launch/boustrophedon.launch.py
@@ -2,6 +2,7 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
+import yaml
 
 def generate_launch_description():
     config = os.path.join(
@@ -9,7 +10,15 @@ def generate_launch_description():
         'config',
         'boustrophedon_params.yaml'
     )
-    
+
+    with open(config, 'r') as f:
+        config_file = yaml.load(f, Loader=yaml.FullLoader)
+    params = config_file["/**"]['ros__parameters']['lawnmower_controller']
+    print("Launching Boustrophedon Navigator with the following parameters:")
+    for key, value in params.items():
+        print(f"  {key}: {value}")
+
+
     return LaunchDescription([
         # Start the turtlesim node
         Node(
@@ -17,13 +26,14 @@ def generate_launch_description():
             executable='turtlesim_node',
             name='turtlesim'
         ),
+
         
         # Start our boustrophedon controller
         Node(
             package='first_order_boustrophedon_navigator',
             executable='boustrophedon_controller',
             name='lawnmower_controller',
-            parameters=[config],
+            parameters=[params],
             output='screen'
         )
     ]) 
